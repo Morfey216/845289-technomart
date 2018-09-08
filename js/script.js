9bfb2d44c6260ac1278369d96f99f692c5f31551
Morfey216 <Morfey4web@gmail.com>
2018-09-08 22:35:50 +0300

    Показ/скрытие карты

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ var write_btn = document.querySelector(".contacts-button");
 var write_popap = document.querySelector(".modal-write");
 var write_close = write_popap.querySelector(".modal-close");
 
+var map_btn = document.querySelector(".small-map");
+var map_popap = document.querySelector(".modal-map");
+var map_close = map_popap.querySelector(".modal-close");
+
 write_btn.addEventListener("click",function(evt){
   evt.preventDefault();
   write_popap.classList.add("modal-show");
@@ -11,3 +15,14 @@ write_close.addEventListener("click",function(evt){
   evt.preventDefault();
   write_popap.classList.remove("modal-show");
 });
+
+map_btn.addEventListener("click",function(evt){
+  evt.preventDefault();
+  map_popap.classList.add("modal-show");
+});
+
+map_close.addEventListener("click",function(evt){
+  evt.preventDefault();
+  map_popap.classList.remove("modal-show");
+});
+
